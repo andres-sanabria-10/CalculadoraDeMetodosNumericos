41b82fd1594174c169cafa5415fba85c301ef96b
JeimmyValderrama <86479248+JeimmyValderrama@users.noreply.github.com>
2024-10-02 11:25:43 -0500

    Actualizacion Punto_Fijo
    Dejar solo el metodo de punto fijo

--- calculadora_metodos/routes.py
+++ calculadora_metodos/routes.py
@@ -31,23 +31,3 @@ def punto_fijo():
     else:
         return jsonify(result), 400
     
-
-@api.route('/biseccion', methods=['POST'])
-def biseccion_route():
-    data = request.json
-    func_input = data.get('func_input')
-    Xi = data.get('Xi', 0)
-    Xu = data.get('Xu', 0)
-    error = data.get('error', 1e-6)
-
-    if func_input is None:
-        return jsonify({"error": "Se debe proporcionar la función f(x) como un string."}), 400
-
-    try:
-        raiz, iteraciones = biseccion_controller(func_input, Xi, Xu, error)
-        return jsonify({
-            "raiz_encontrada": raiz,
-            "iteraciones": iteraciones
-        })
-    except ValueError as e:
-        return jsonify({"error": str(e)}), 400
