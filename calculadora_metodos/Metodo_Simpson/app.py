cf07d18d63271ad92b9cb4f2eb560dff26ca4ea7
TatianaLesmes <yennyfer.lesmes@uptc.edu.co>
2024-12-01 10:25:40 -0500

    ActualizacionTrapecio,simpson

--- calculadora_metodos/Metodo_Simpson/app.py
+++ calculadora_metodos/Metodo_Simpson/app.py
@@ -1,8 +1,10 @@
 import numpy as np
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import sympy as sp
 
 app = Flask(__name__)
+CORS(app)
 
 # Función para evaluar la función en un valor x
 def evaluar_funcion(funcion_str, x):
